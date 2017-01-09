29fe9222deff1dfcfbeecb5c4f7c8c2bfa764c6c
EMMANUEL505 <EMMANUELSIGALA@HOTMAIL.COM>
2017-01-09 14:31:21 -0700

    >First GUI design >Only write function

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -45,17 +45,19 @@
             // 
             // cmbComs
             // 
+            this.cmbComs.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbComs.FormattingEnabled = true;
             this.cmbComs.Location = new System.Drawing.Point(271, 52);
             this.cmbComs.Name = "cmbComs";
-            this.cmbComs.Size = new System.Drawing.Size(155, 21);
+            this.cmbComs.Size = new System.Drawing.Size(205, 32);
             this.cmbComs.TabIndex = 0;
             // 
             // btnSelect
             // 
+            this.btnSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSelect.Location = new System.Drawing.Point(52, 24);
             this.btnSelect.Name = "btnSelect";
-            this.btnSelect.Size = new System.Drawing.Size(125, 37);
+            this.btnSelect.Size = new System.Drawing.Size(195, 37);
             this.btnSelect.TabIndex = 1;
             this.btnSelect.Text = "Import (xls) file...";
             this.btnSelect.UseVisualStyleBackColor = true;
@@ -78,17 +80,21 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(327, 36);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(312, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.Size = new System.Drawing.Size(96, 24);
             this.label1.TabIndex = 3;
             this.label1.Text = "Com Port";
             // 
             // btnOpen
             // 
-            this.btnOpen.Location = new System.Drawing.Point(271, 75);
+            this.btnOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpen.Location = new System.Drawing.Point(271, 87);
             this.btnOpen.Name = "btnOpen";
-            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.Size = new System.Drawing.Size(98, 37);
             this.btnOpen.TabIndex = 4;
             this.btnOpen.Text = "Open Port";
             this.btnOpen.UseVisualStyleBackColor = true;
@@ -97,9 +103,10 @@
             // btnClose
             // 
             this.btnClose.Enabled = false;
-            this.btnClose.Location = new System.Drawing.Point(351, 75);
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(378, 87);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.Size = new System.Drawing.Size(98, 37);
             this.btnClose.TabIndex = 4;
             this.btnClose.Text = "Close Port";
             this.btnClose.UseVisualStyleBackColor = true;
@@ -107,9 +114,10 @@
             // 
             // btnRead
             // 
-            this.btnRead.Location = new System.Drawing.Point(271, 150);
+            this.btnRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRead.Location = new System.Drawing.Point(271, 182);
             this.btnRead.Name = "btnRead";
-            this.btnRead.Size = new System.Drawing.Size(121, 23);
+            this.btnRead.Size = new System.Drawing.Size(98, 37);
             this.btnRead.TabIndex = 4;
             this.btnRead.Text = "Read numbers";
             this.btnRead.UseVisualStyleBackColor = true;
@@ -117,9 +125,10 @@
             // 
             // btnWrite
             // 
-            this.btnWrite.Location = new System.Drawing.Point(271, 121);
+            this.btnWrite.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWrite.Location = new System.Drawing.Point(271, 135);
             this.btnWrite.Name = "btnWrite";
-            this.btnWrite.Size = new System.Drawing.Size(121, 23);
+            this.btnWrite.Size = new System.Drawing.Size(98, 37);
             this.btnWrite.TabIndex = 4;
             this.btnWrite.Text = "Write numbers";
             this.btnWrite.UseVisualStyleBackColor = true;
@@ -140,7 +149,10 @@
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnSelect);
             this.Controls.Add(this.cmbComs);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(640, 460);
+            this.MinimumSize = new System.Drawing.Size(640, 460);
             this.Name = "Form1";
             this.Text = "GSM relay ";
             this.TopMost = true;
